64ee470eef9e8fcba59df0d4309c969f76afd40b
davit412v2 <luisdavidbarrigagaray97@gmail.com>
2025-07-25 11:51:36 -0500

    first commit

---
+++ src/main/java/com/example/catbackend/catbackend/CatbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.catbackend.catbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/catbackend/catbackend/CatbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.catbackend.catbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
